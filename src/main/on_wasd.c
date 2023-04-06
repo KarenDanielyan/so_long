dbe17f7d44be28f71663bbef0c323a8634aafc5a
KarenDanielyan <karendanielyan66@gmail.com>
2023-04-06 17:36:24 +0400

    we ditch mac cuz fuckin intellisence doesn't work

--- src/main/on_wasd.c
+++ src/main/on_wasd.c
@@ -6,18 +6,39 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 21:12:59 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/04/05 19:14:17 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:32:03 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "events.h"
 #include <ft_printf.h>
 
+typedef struct	s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+static void	on_w(t_game *game)
+{
+	t_point		p;
+	t_tile		**tiles;
+	t_player	*player;
+	//int			i;
+
+	player = game->player;
+	tiles = game->map->tiles;
+	p.x = player->x;
+	p.y = player->y;
+	game
+	mlx_put_image_to_window(game->mlx, game->window, player->assets->mv_up[0], p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
+}
+
 int	on_player_move(int key, t_game *game)
 {
 	(void)game;
 	if (key == W)
-		ft_printf("I will move up soon...\n");
+		on_w(game);
 	if (key == A)
 		ft_printf("I will move left soon...\n");
 	if (key == S)
