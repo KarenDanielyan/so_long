ef83d047ce91326aef7b73d4de197e360d23924c
KarenDanielyan <karendanielyan66@gmail.com>
2023-04-06 22:15:40 +0400

    I can move but my animations are messy

--- src/main/on_wasd.c
+++ src/main/on_wasd.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 21:12:59 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/04/06 21:57:10 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/04/06 22:11:10 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,13 +82,13 @@ static void	on_s(t_game *game)
 	p.x = player->x;
 	p.y = player->y;
 	mlx_put_image_to_window(game->mlx, game->window,
-		player->textures->mv_up[0], p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
-	if ((p.y + 1) >= 0 && tiles[p.y + 1][p.x].type != WALLS)
+		player->textures->mv_down[0], p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
+	if ((p.y + 1) <= game->map->height && tiles[p.y + 1][p.x].type != WALLS)
 	{
 		mlx_put_image_to_window(game->mlx, game->window,
 			tiles[p.y][p.x].texture, p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
 		mlx_put_image_to_window(game->mlx, game->window,
-			player->textures->mv_up[1], p.x * TEXTURE_SIZE, (p.y + 1) * TEXTURE_SIZE);
+			player->textures->mv_down[1], p.x * TEXTURE_SIZE, (p.y + 1) * TEXTURE_SIZE);
 		player->y = p.y + 1;
 		if (tiles[p.y + 1][p.x].symb == 'C')
 			get_coll_by_loc(game->cols, game->c_count, (p.y + 1), p.x)->is_collected = 1;
@@ -108,16 +108,16 @@ static void	on_d(t_game *game)
 	p.x = player->x;
 	p.y = player->y;
 	mlx_put_image_to_window(game->mlx, game->window,
-		player->textures->mv_up[0], p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
+		player->textures->mv_right[0], p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
 	if ((p.x + 1) <= game->map->width && tiles[p.y][p.x + 1].type != WALLS)
 	{
 		mlx_put_image_to_window(game->mlx, game->window,
 			tiles[p.y][p.x].texture, p.x * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
 		mlx_put_image_to_window(game->mlx, game->window,
-			player->textures->mv_up[1], (p.x + 1) * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
+			player->textures->mv_right[1], (p.x + 1) * TEXTURE_SIZE, p.y * TEXTURE_SIZE);
 		player->x = p.x + 1;
 		if (tiles[p.y][p.x + 1].symb == 'C')
-			get_coll_by_loc(game->cols, game->c_count, p.y, (p.x + 1))->is_collected = 1;
+			get_coll_by_loc(game->cols, game->c_count, (p.x + 1), p.y)->is_collected = 1;
 		tiles[p.y][p.x].symb = tiles[p.y][p.x].type;
 		tiles[p.y][p.x + 1].symb = 'P';
 	}
@@ -125,7 +125,6 @@ static void	on_d(t_game *game)
 
 int	on_player_move(int key, t_game *game)
 {
-	(void)game;
 	if (key == W)
 		on_w(game);
 	if (key == A)
